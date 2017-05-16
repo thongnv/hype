01ce1fc9a24811bec7d3e706b891d2a15bfe64a0
Nguyen Van Thong <nvthong@cmc.com.vn>
2017-05-16 15:21:31 +0700

    Refactor with code style

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,115 +1,112 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions} from "@angular/http";
-import {AppSetting} from '../app.setting';
-import {LocalStorageService} from 'angular-2-local-storage';
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { AppSetting } from '../app.setting';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Injectable()
 export class MainService {
 
-    constructor(private _localStorageService: LocalStorageService, private _http: Http) {
-    }
+  constructor(private _localStorageService: LocalStorageService, private _http: Http) {
+  }
 
-    login(fb_token: string) {
-        // this._cookieService.removeAll();
-        return new Promise((resolve, reject) => {
+  public login(fbToken: string) {
+    return new Promise((resolve, reject) => {
 
-            let token = {fb_token: fb_token};
-            let headers = new Headers({"Content-Type": "application/json"});
-            let options = new RequestOptions({headers: headers, withCredentials: true});
+      let token = {fb_token: fbToken};
+      let headers = new Headers({'Content-Type': 'application/json'});
+      let options = new RequestOptions({headers, withCredentials: true});
 
-            this._http.post(AppSetting.API_LOGIN, JSON.stringify(token), options).subscribe(
-                (response) => {
-                    console.log('login#success', response.json());
-                    this._localStorageService.set("csrf_token", response.json().csrf_token);
-                    this._localStorageService.set("logout_token", response.json().logout_token);
-                    this._localStorageService.set("user_name", response.json().current_user.name);
-                    this._localStorageService.set("uid", response.json().current_user.uid);
+      this._http.post(AppSetting.API_LOGIN, JSON.stringify(token), options).subscribe(
+        (response) => {
+          console.log('login#success', response.json());
+          this._localStorageService.set('csrf_token', response.json().csrf_token);
+          this._localStorageService.set('logout_token', response.json().logout_token);
+          this._localStorageService.set('user_name', response.json().current_user.name);
+          this._localStorageService.set('uid', response.json().current_user.uid);
 
-                    resolve(response);
-                },
-                (err) => {
-                    console.debug(err);
-                    reject(err);
-                }
-            );
-        });
-    }
+          resolve(response);
+        },
+        (err) => {
+          console.debug(err);
+          reject(err);
+        }
+      );
+    });
+  }
 
-    logout() {
-        let logout_token = this._localStorageService.get("logout_token");
-        let headers = new Headers({"Content-Type": "application/json"});
-        let options = new RequestOptions({headers: headers, withCredentials: true});
+  public logout() {
+    let logoutToken = this._localStorageService.get('logout_token');
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers, withCredentials: true});
 
-        return new Promise((resolve, reject) => {
-            this._http.get(AppSetting.API_LOGOUT + logout_token, options).subscribe(
-                (response) => {
-                    console.log("API_LOGOUT: ", response.json());
-                    this._localStorageService.clearAll();
-                    resolve(response);
-                },
-                (err) => {
-                    console.debug(err);
-                    reject(err);
-                }
-            );
-        });
-    }
+    return new Promise((resolve, reject) => {
+      this._http.get(AppSetting.API_LOGOUT + logoutToken, options).subscribe(
+        (response) => {
+          console.log('API_LOGOUT: ', response.json());
+          this._localStorageService.clearAll();
+          resolve(response);
+        },
+        (err) => {
+          console.debug(err);
+          reject(err);
+        }
+      );
+    });
+  }
 
-    getUserProfile() {
-        let csrf_token = <string>this._localStorageService.get("csrf_token");
-        let headers = new Headers({"Content-Type": "application/json", "X-CSRF-Token": csrf_token});
-        let options = new RequestOptions({headers: headers, withCredentials: true});
+  public getUserProfile() {
+    let csrfToken = <string> this._localStorageService.get('csrf_token');
+    let headers = new Headers({'Content-Type': 'application/json', 'X-CSRF-Token': csrfToken});
+    let options = new RequestOptions({headers, withCredentials: true});
 
-        return new Promise((resolve, reject) => {
-            this._http.get(AppSetting.API_USER_PROFILE, options).subscribe(
-                (response) => {
-                    console.log("USER PRO: ", response.json());
-                    resolve(response);
-                },
-                (err) => {
-                    console.debug(err);
-                    reject(err);
-                }
-            );
-        });
-    }
+    return new Promise((resolve, reject) => {
+      this._http.get(AppSetting.API_USER_PROFILE, options).subscribe(
+        (response) => {
+          console.log('USER PRO: ', response.json());
+          resolve(response);
+        },
+        (err) => {
+          console.debug(err);
+          reject(err);
+        }
+      );
+    });
+  }
 
-    setUserProfile(userProfile: any) {
-        let csrf_token = <string>this._localStorageService.get("csrf_token");
-        let headers = new Headers({"Content-Type": "application/json", "X-CSRF-Token": csrf_token});
-        let options = new RequestOptions({headers: headers, withCredentials: true});
-        console.log(userProfile);
-        return new Promise((resolve, reject ) => {
-            this._http.post(AppSetting.API_USER_PROFILE,userProfile,  options).
-            subscribe(
-                (response) => {
-                    console.log("USER PRO: ", response.json());
-                    resolve(response);
-                },
-                (err) => {
-                    console.debug(err);
-                    reject(err);
-                }
-            );
-        });
-    }
+  public setUserProfile(userProfile: any) {
+    let csrfToken = <string> this._localStorageService.get('csrf_token');
+    let headers = new Headers({'Content-Type': 'application/json', 'X-CSRF-Token': csrfToken});
+    let options = new RequestOptions({headers, withCredentials: true});
+    console.log(userProfile);
+    return new Promise((resolve, reject) => {
+      this._http.post(AppSetting.API_USER_PROFILE, userProfile, options).subscribe(
+        (response) => {
+          console.log('USER PRO: ', response.json());
+          resolve(response);
+        },
+        (err) => {
+          console.debug(err);
+          reject(err);
+        }
+      );
+    });
+  }
 
-    isLoggedIn(): any {
-        let csrf_token = <string>this._localStorageService.get("csrf_token");
-        let headers = new Headers({"Content-Type": "application/json"});
-        let options = new RequestOptions({headers: headers, withCredentials: true});
+  public isLoggedIn(): any {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers, withCredentials: true});
 
-        return new Promise((resolve, reject) => {
-            this._http.get(AppSetting.API_LOGIN_STATUS, options).subscribe(
-                (response) => {
-                    console.log("API_LOGIN_STATUS: ", response.json());
-                    resolve(response);
-                },
-                (err) => {
-                    console.debug(err);
-                    reject(err);
-                }
-            );
-        });
-    }
+    return new Promise((resolve, reject) => {
+      this._http.get(AppSetting.API_LOGIN_STATUS, options).subscribe(
+        (response) => {
+          console.log('API_LOGIN_STATUS: ', response.json());
+          resolve(response);
+        },
+        (err) => {
+          console.debug(err);
+          reject(err);
+        }
+      );
+    });
+  }
 }
