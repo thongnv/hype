a1a1b6b2cb023608ee5cbdb16964846865f9a0bb
DinhQuyet <dinhquyet.92@gmail.com>
2017-05-30 15:42:41 +0700

    - add curate list mock data

--- src/app/curate/curate.component.ts
+++ src/app/curate/curate.component.ts
@@ -7,21 +7,25 @@ import { MainService } from '../services/main.service';
   styleUrls: ['./curate.component.css']
 })
 export class CurateComponent implements OnInit {
-
   public data: any;
-  public articles: any;
-
+  public articles: any[];
+  public categories: any[];
+  public trending: any[];
+  public selectedCategory: any;
   public constructor(private mainService: MainService) {
   }
-
   public ngOnInit() {
     this.data = {lat: 1.390570, lng: 103.351923};
-    this.getArticles('latest');
+    this.getArticles();
   }
-
-  public getArticles(type: string): void {
-    // this.mainService.getArticle(type).then((resp) => {
-    //   this.articles = resp;
-    // });
+  public getArticles(): void {
+    this.mainService.getUserPublicProfile().then((resp) => {
+      this.articles = resp.curate_list;
+      this.categories = resp.categories;
+      this.trending = resp.trending;
+    });
+  }
+  public onSelectCategory(cat: any){
+    this.selectedCategory = cat;
   }
 }
