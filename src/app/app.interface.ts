c5924b8c3c007cde7f4169c79fde8524238f0ad1
dddoi <dodinhdoi@gmail.com>
2017-06-05 18:01:37 +0700

    Fix error gmap

--- src/app/app.interface.ts
+++ src/app/app.interface.ts
@@ -69,3 +69,12 @@ export interface EventType {
   iconUrl: string;
   selected: boolean;
 }
+
+export interface FileReaderEventTarget extends EventTarget {
+  result: string;
+}
+
+export interface FileReaderEvent extends Event {
+  target: FileReaderEventTarget;
+  getMessage(): string;
+}
