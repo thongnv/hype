6c2eb977a0303350c1cb0ce8c6596e4c03a29578
dtthu@cmc.com.vn <linhtinh>
2017-06-13 17:34:09 +0700

    add data to category mode eat,play

--- src/app/services/mode.service.ts
+++ src/app/services/mode.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BaseApiService } from "./service_base.service";
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
+import {objectify} from "tslint/lib/utils";
 
 @Injectable()
 
@@ -11,15 +12,23 @@ export class ModeService {
     console.log('category service');
   }
 
-  public getCategories(params: any) {
-    let seq = this.api.get('http://hylo.dev/assets/mock-data/terms.json').share();
-    seq
-      .map(res => res.json())
-      .subscribe(res => {
-      }, err => {
-        console.error('ERROR', err);
-      });
-    return seq;
+  public getCategories(params) {
+      let seq = this.api.get('http://hylo.dev/assets/mock-data/terms.json').share();
+      switch (params){
+        case 'eat':
+             seq = this.api.get('http://hylo.dev/assets/mock-data/term_eat.json').share();
+            break;
+        case 'play':
+             seq = this.api.get('http://hylo.dev/assets/mock-data/term_play.json').share();
+            break;
+    }
+      seq
+          .map(res => res.json())
+          .subscribe(res => {
+          }, err => {
+              console.error('ERROR', err);
+          });
+      return seq;
   }
 
   public getModes(params: any) {
