228417107b5650360406977d7dc2ca12ed28e9c0
DinhQuyet <dinhquyet.92@gmail.com>
2017-06-16 13:44:12 +0700

    - Update User update profile UI

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -41,6 +41,7 @@ export class NavbarComponent implements OnInit {
   }
 
   public ngOnInit() {
+    let nbList = ['Alexandra', 'Aljunied', 'Amoy Street', 'Ang Mo Kio', 'Balestier', 'Bartley', 'Bayfront', 'Beach Road', 'Beauty World', 'Bedok', 'Bedok Reservoir', 'Bencoolen', 'Bishan', 'Boat Quay', 'Boon Keng', 'Boon Lay', 'Botanic Gardens', 'Braddell', 'Bras Basah', 'Buangkok', 'Bugis', 'Bukit Batok', 'Bukit Gombak', 'Bukit Merah', 'Bukit Panjang', 'Bukit Timah', 'Buona Vista', 'Changi', 'Changi Airport', 'China Square Central', 'Chinatown', 'Chinese Garden', 'Chip Bee Gardens', 'Choa Chu Kang', 'Circular Road', 'City Hall', 'Clarke Quay', 'Clementi', 'Club Street', 'Commonwealth', 'Coronation', 'Dakota', 'Dempsey', 'Dhoby Ghaut', 'Dover', 'Downtown', 'Duxton', 'East Coast Parkway', 'Esplanade', 'Eunos', 'Everton Park', 'Expo', 'Farrer Park', 'Farrer Road', 'Fort Canning', 'Gardens by the Bay', 'Geylang', 'Geylang Bahru', 'Great World', 'Haji Lane', 'Harbourfront', 'Hillview', 'Holland Village', 'Hougang', 'Jalan Besar', 'Jalan Kayu', 'Jalan Riang', 'Joo Chiat', 'Jurong East', 'Jurong West', 'Kaki Bukit', 'Kallang', 'Katong', 'Kembangan', 'Kent Ridge', 'Khatib', 'King Albert Park', 'Kovan', 'Labrador', 'Lakeside', 'Lavender', 'Little India', 'Lorong Kilat', 'Macpherson', 'Marina Bay', 'Marina Square', 'Marine Parade', 'Marymount', 'Maxwell', 'Millenia Walk', 'Mountbatten', 'Newton', 'North Bridge Road', 'Novena', 'Old Airport Road', 'One North', 'Orchard', 'Orchard Road', 'Outram Park', 'Pasir Panjang', 'Pasir Ris', 'Paya Lebar', 'Portsdown', 'Potong Pasir', 'Promenade', 'Punggol', 'Punggol Waterway', 'Queenstown', 'Raffles Place', 'Rangoon Road', 'Redhill', 'River Valley', 'Robertson Quay', 'Rochor', 'Seletar', 'Sembawang', 'Sengkang', 'Sentosa', 'Serangoon', 'Serangoon Gardens', 'Shenton Way', 'Siglap', 'Simei', 'Simpang Bedok', 'Sixth Avenue', 'Somerset', 'Stadium', 'Stevens', 'Tai Seng', 'Tampines', 'Tanah Merah', 'Tanglin', 'Tanjong Katong', 'Tanjong Pagar', 'Telok Ayer', 'Telok Blangah', 'Thomson', 'Tiong Bahru', 'Toa Payoh', 'Tuas', 'Upper Bukit Timah', 'Upper East Coast Road', 'West Coast', 'Woodlands', 'Yio Chu Kang', 'Yishun'];
     this.loginData = this.localStorageService.get('loginData') ?
       JSON.parse(<string> this.localStorageService.get('loginData')) : null;
     console.log('local storage: ', this.loginData);
@@ -48,8 +49,10 @@ export class NavbarComponent implements OnInit {
     this.userInfo = this.appState.state.userInfo;
     this.mapOptions = [
       {id: 1, name: 'Singapore'},
-      {id: 2, name: 'Neighbourhood'}
     ];
+    nbList.forEach((item, index) => {
+      this.mapOptions.push({id: index + 2, name: item});
+    });
     this.selectedMapOption = this.mapOptions[0];
     if (this.loginData) {
       this.getNotifications();
