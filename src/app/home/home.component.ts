2dd145f61f59364a2f8fef42c99fec3db2038888
mrvinhdv <duongvinh.itdv3@gmail.com>
2017-06-16 16:52:45 +0700

    add module home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -180,7 +180,7 @@ export class HomeComponent implements OnInit {
             this.msgContent = res.message;
         }, err=> {
             if (err.status == 403) {
-                //this.route.navigate(['login']);
+                this.route.navigate(['login']);
             } else {
                 this.alertType = 'error';
                 this.msgContent = 'Sorry, bookmark error please try again';
