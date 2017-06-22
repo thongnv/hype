d805727271acbb03dae4149a953e4280fa66809c
DinhQuyet <dinhquyet.92@gmail.com>
2017-06-22 10:27:53 +0700

    - Update logout - Add local google font

--- src/app/auth/logout/logout.component.ts
+++ src/app/auth/logout/logout.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MainService } from '../../services/main.service';
 import { AppState } from '../../app.service';
 import { LocalStorageService } from 'angular-2-local-storage';
 import { LoaderService } from '../../shared/loader/loader.service';
@@ -11,22 +10,20 @@ import { LoaderService } from '../../shared/loader/loader.service';
 })
 export class LogoutComponent implements OnInit {
 
-  public constructor(private mainService: MainService,
+  public constructor(
                      private localStorageService: LocalStorageService,
-                     private appState: AppState, private loaderService: LoaderService) {
+                     private appState: AppState,
+                     private loaderService: LoaderService) {
     this.loaderService.show();
   }
 
   public logout(): void {
-    this.mainService.logout().then(
-      (resp) => {
-        console.log(resp);
-        if (resp.ok) {
-          window.location.href = '/';
-        }
-        this.loaderService.hide();
-      }
-    );
+    this.localStorageService.clearAll();
+    this.appState.set('userInfo', null);
+    setTimeout(() => {
+      this.loaderService.hide();
+      window.location.href = '/';
+    }, 1000);
   }
 
   public ngOnInit() {
