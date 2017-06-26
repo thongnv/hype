30dad93ca0e82fbc5d3d8180e9e61a3c63101335
dddoi <dodinhdoi@gmail.com>
2017-06-26 09:44:20 +0700

    Format code

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,59 +1,61 @@
 import { Injectable } from '@angular/core';
-import { BaseApiService } from "./service_base.service";
-import {AppSetting} from "../app.setting";
+import { BaseApiService } from './service_base.service';
+import { AppSetting } from '../app.setting';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-import {map} from "rxjs/operator/map";
-import {$SQ} from "codelyzer/angular/styles/chars";
-
+import { map } from 'rxjs/operator/map';
+import { $SQ } from 'codelyzer/angular/styles/chars';
 
 @Injectable()
 
 export class HomeService {
 
-    constructor(private api:BaseApiService) {
-        console.log('home api');
-    }
+  constructor(private api: BaseApiService) {
+    console.log('home api');
+  }
 
-    getEvents(params:any) {
-        let seq = this.api.get(AppSetting.API_TRENDING, params).share();
-        seq.map(res=>res.json())
-            .subscribe(res=> {
+  public getEvents(params: any) {
+    let seq = this.api.get(AppSetting.API_TRENDING, params).share();
+    seq.map((res) => res.json())
+      .subscribe((res) => {
+        //
+      }, (err) => {
+        console.log('err', err);
+      });
+    return seq;
+  }
 
-            }, err=> {
-                console.log('err', err);
-            });
-        return seq;
-    }
+  public getTop100(params: any) {
+    let seq = this.api.get(AppSetting.API_ENDPOINT_TOP, params).share();
+    seq.map((res) => res.json())
+      .subscribe((res) => {
+        //
+      }, (err) => {
+        console.log('err', err);
+      });
+    return seq;
+  }
 
-    getTop100(params:any){
-        let seq = this.api.get(AppSetting.API_ENDPOINT_TOP, params).share();
-        seq.map(res=>res.json())
-            .subscribe(res=> {
-            }, err=> {
-                console.log('err', err);
-            });
-        return seq;
-    }
+  public getCategories(type: any) {
+    let seq = this.api.get(AppSetting.API_CATEGORIES_EVENT).share();
+    seq.map((res) => res.json())
+      .subscribe((res) => {
+        //
+      }, (err) => {
+        //
+      });
+    return seq;
+  }
 
-    getCategories(type:any) {
-        let seq = this.api.get(AppSetting.API_CATEGORIES_EVENT).share();
-        seq.map(res=>res.json())
-            .subscribe(res=> {
-            }, err=> {
-                console.log(err);
-            });
-        return seq;
-    }
+  public likeEvent(body: any) {
 
-    likeEvent(body:any) {
-
-        let seq = this.api.post(AppSetting.API_ENDPOINT_LIKE + '&slug=' + body.slug, body).share();
-        seq.map(res=>res.json())
-            .subscribe(res=> {
-            }, err=> {
-
-            });
-        return seq;
-    }
+    let seq = this.api.post(AppSetting.API_ENDPOINT_LIKE + '&slug=' + body.slug, body).share();
+    seq.map((res) => res.json())
+      .subscribe((res) => {
+        //
+      }, (err) => {
+        //
+      });
+    return seq;
+  }
 }
