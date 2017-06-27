fcda95ab01ee99610403bd28eb50b785156ff282
David Nguyen <thanhngvpt@gmail.com>
2017-06-27 10:35:00 +0700

    [add] SEOService to setting meta tags for page

---
+++ src/app/services/seo.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Title, Meta} from '@angular/platform-browser';
+
+@Injectable()
+export class SeoService {
+
+  constructor(private title: Title, private meta: Meta) { }
+
+  /**
+   * Set SEO meta tags for web page
+   * @param title: title of the page
+   * @param author: author of the page
+   * @param keywords: keywords for search engine
+   * @param description: short description of the page
+   */
+  setSEOMetaTags(title: string, author: string, keywords: string, description: string) {
+    this.title.setTitle(title);
+    this.meta.addTags([
+      {name: 'author', content: author},
+      {name: 'keywords', content: keywords},
+      {name: 'description', content: description},
+    ]);
+  }
+
+}
