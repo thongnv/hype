dc641eae3be7ddc8111a54546a3168e5098d89ca
Tom Nguyen <thongola@hotmail.com>
2017-07-27 18:43:24 +0700

    home page - added no-result-found image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,6 +54,7 @@ export class HomeComponent implements OnInit {
   public shownotfound: boolean = false;
   public layoutWidth: number;
   public innerWidth: number;
+  public loading = true;
 
   private stopped: boolean = false;
   private zoomChanged: boolean = false;
@@ -529,6 +530,7 @@ export class HomeComponent implements OnInit {
 
   private getTop100(params) {
     console.log(params);
+    this.loading = true;
     this.homeService.getTop100(params).map((resp) => resp.json()).subscribe(
       (resp) => {
         this.total = resp.total;
@@ -548,6 +550,7 @@ export class HomeComponent implements OnInit {
         this.loaderService.hide();
         this.smallLoader.hide();
         this.zoomChanged = false;
+        this.loading = false;
       },
       (err) => {
         console.log(err);
@@ -557,10 +560,12 @@ export class HomeComponent implements OnInit {
         this.total = 0;
         this.loaderService.hide();
         this.smallLoader.hide();
+        this.loading = false;
       });
   }
 
   private getEvents(params) {
+    this.loading = true;
     this.homeService.getEvents(params).map((response) => response.json()).subscribe(
       (response) => {
         if (this.loadMore) {
@@ -579,6 +584,7 @@ export class HomeComponent implements OnInit {
         this.loadMore = false;
         this.loaderService.hide();
         this.smallLoader.hide();
+        this.loading = false;
       },
       (err) => {
         console.log(err);
@@ -586,6 +592,7 @@ export class HomeComponent implements OnInit {
         this.endRecord = false;
         this.loaderService.hide();
         this.smallLoader.hide();
+        this.loading = false;
       });
   }
 }
