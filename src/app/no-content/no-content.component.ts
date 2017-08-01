dddd4f1f90f4abfe6ad8638920ad9d3942324656
Do Trong Luc <dtluc@cmc.com.vn>
2017-08-01 10:32:25 +0700

    update go home link

--- src/app/no-content/no-content.component.ts
+++ src/app/no-content/no-content.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <div id="notFoundDiv" class="not-found">
         <div><div class="sprite sprite-no-results-found-2x" style="display: inline-block"></div></div>
         <h2>Page not Found</h2>
-        <div>Sorry the page does not existed please <a [routerLink]="['./home']" class="clear_filter">go home</a>.</div>
+        <div>Sorry the page does not existed please <a [routerLink]="['/']" class="clear_filter">go home</a>.</div>
       </div>
     </div>
   `
