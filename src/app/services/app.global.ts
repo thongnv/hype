fc4e0e898f9dcee295c0af2e876784ce5a6546e4
dddoi <dodinhdoi@gmail.com>
2017-09-27 10:40:57 +0700

    Add default marker event

--- src/app/services/app.global.ts
+++ src/app/services/app.global.ts
@@ -11,19 +11,20 @@ export class AppGlobals {
   public toggleMap = false;
 
   public eventIcon = [
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/art.png', name: 'art'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/camp.png', name: 'camp'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/dating.png', name: 'dating'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/education.png', name: 'education'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/food-promo.png', name: 'food promo'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/gaming.png', name: 'gaming'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/live-performance.png', name: 'life performance'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/music.png', name: 'music'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/night-life.png', name: 'nightlife'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/pop-up-fair.png', name: 'pop-up fair'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/shopping.png', name: 'shopping'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/sports.png', name: 'sports'},
-    {url: 'http://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/tech.png', name: 'tech'}];
+    {url: 'https://hylo.sg/assets/icon/locationmarker.png', name: 'default'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/art.png', name: 'art'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/camp.png', name: 'camp'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/dating.png', name: 'dating'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/education.png', name: 'education'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/food-promo.png', name: 'food promo'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/gaming.png', name: 'gaming'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/live-performance.png', name: 'life performance'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/music.png', name: 'music'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/night-life.png', name: 'nightlife'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/pop-up-fair.png', name: 'pop-up fair'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/shopping.png', name: 'shopping'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/sports.png', name: 'sports'},
+    {url: 'https://s3-ap-southeast-1.amazonaws.com/live.hylosg/avatar/markers/tech.png', name: 'tech'}];
 
   public neighbourhoodStorage = new BehaviorSubject<HyloLocation>({name: 'Singapore', lat: 1.290270, lng: 103.851959});
   @Output() public typeChangeEmitter: EventEmitter<any> = new EventEmitter();
